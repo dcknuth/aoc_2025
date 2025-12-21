855487814ea8c348e4b54089c0c3f7b9bb12f940
dcknuth <36804076+dcknuth@users.noreply.github.com>
2025-12-21 14:08:25 -0800

    Day9 Rust V2
    i32 instead of i64

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn part1(s: &String) -> i64 {
-    let points: Vec<Vec<i64>> = s.lines()
+    let points: Vec<Vec<i32>> = s.lines()
         .map(|l| l.split(',')
-            .map(|n| n.parse::<i64>().unwrap()).collect())
+            .map(|n| n.parse::<i32>().unwrap()).collect())
             .collect();
     
-    let mut max_area = 0;
+    let mut max_area: i64 = 0;
     for i in 0..points.len()-1 {
         for j in i+1..points.len() {
-            let cur_area = ((points[i][0] - points[j][0]).abs() + 1) *
-                ((points[i][1] - points[j][1]).abs() + 1);
+            let cur_area: i64 = ((points[i][0] - points[j][0]).abs() + 1) as i64 *
+                ((points[i][1] - points[j][1]).abs() + 1) as i64;
             if cur_area > max_area {
                 max_area = cur_area;
             }
@@ -21,39 +21,39 @@ pub fn part1(s: &String) -> i64 {
 use itertools::Itertools;
 
 pub fn part2(s: &String) -> i64 {
-    let points: Vec<Vec<i64>> = s.lines()
+    let points: Vec<Vec<i32>> = s.lines()
         .map(|l| l.split(',')
-            .map(|n| n.parse::<i64>().unwrap()).collect())
+            .map(|n| n.parse::<i32>().unwrap()).collect())
             .collect();
-    let mut lines: Vec<Vec<&Vec<i64>>> = Vec::new();
+    let mut lines: Vec<Vec<&Vec<i32>>> = Vec::new();
     for (a, b) in points.iter().cycle().tuple_windows()
         .take(points.len()) {
             lines.push(vec![a, b]);
         }
     
-    let mut max_area = 0;
+    let mut max_area: i64 = 0;
     for i in 0..points.len()-1 {
         for j in i+1..points.len() {
-            let cur_area = ((points[i][0] - points[j][0]).abs() + 1) *
-                    ((points[i][1] - points[j][1]).abs() + 1);
+            let cur_area: i64 = ((points[i][0] - points[j][0]).abs() + 1) as i64 *
+                    ((points[i][1] - points[j][1]).abs() + 1) as i64;
             if cur_area > max_area {
                 // for each rectangle, see if a line passes inside
                 let mut intersect = false;
                 for line in &lines {
-                    if i64::max(line[0][0], line[1][0]) <=
-                        i64::min(points[i][0], points[j][0]) {
+                    if i32::max(line[0][0], line[1][0]) <=
+                        i32::min(points[i][0], points[j][0]) {
                             continue;
                     }
-                    if i64::min(line[0][0], line[1][0]) >=
-                        i64::max(points[i][0], points[j][0]) {
+                    if i32::min(line[0][0], line[1][0]) >=
+                        i32::max(points[i][0], points[j][0]) {
                             continue;
                     }
-                    if i64::max(line[0][1], line[1][1]) <=
-                        i64::min(points[i][1], points[j][1]) {
+                    if i32::max(line[0][1], line[1][1]) <=
+                        i32::min(points[i][1], points[j][1]) {
                             continue;
                     }
-                    if i64::min(line[0][1], line[1][1]) >=
-                        i64::max(points[i][1], points[j][1]) {
+                    if i32::min(line[0][1], line[1][1]) >=
+                        i32::max(points[i][1], points[j][1]) {
                             continue;
                     }
                     intersect = true;
